a3d348882bcfab515ca65c23f21fbdc15ed83838
KCC6128 <kcchen0105@gmail.com>
2026-06-23 00:59:40 +0800

    Add I2C_Interrupt project files

---
+++ I2C/Interrupt/Core/Src/main.c
@@ -0,0 +1,434 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+#define MPU6500_ADDRESS     (0x68 << 1)
+#define WHO_AM_I_MPU6500    0x75
+#define PWR_MGMT_1          0x6B
+#define ACCEL_XOUT_H        0x3B
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart5;
+
+/* USER CODE BEGIN PV */
+
+uint8_t readData = 0;
+uint8_t writeData = 0;
+uint8_t accelData[6] = {0};
+
+int16_t accelX = 0;
+int16_t accelY = 0;
+int16_t accelZ = 0;
+
+char msg[128];
+
+volatile uint8_t accelReady = 0;
+volatile uint8_t i2cError = 0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_UART5_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+static void UART_Print(char *str)
+{
+    HAL_UART_Transmit(&huart5, (uint8_t *)str, strlen(str), 100);
+}
+
+static void Start_Accel_Read_IT(void)
+{
+    HAL_StatusTypeDef ret;
+
+    ret = HAL_I2C_Mem_Read_IT(&hi2c1,
+                              MPU6500_ADDRESS,
+                              ACCEL_XOUT_H,
+                              I2C_MEMADD_SIZE_8BIT,
+                              accelData,
+                              6);
+	
+    if (ret == HAL_OK)
+    {
+        UART_Print("Start I2C interrupt read success.\r\n");
+    }
+
+    if (ret != HAL_OK)
+    {
+        UART_Print("Start I2C interrupt read failed.\r\n");
+    }
+}
+
+void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+    if (hi2c->Instance == I2C1)
+    {
+        accelReady = 1;
+    }
+}
+
+void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c)
+{
+    if (hi2c->Instance == I2C1)
+    {
+        i2cError = 1;
+    }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_UART5_Init();
+  /* USER CODE BEGIN 2 */
+
+	UART_Print("I2C Interrupt Demo Start\r\n\r\n");
+
+	/* Step 1: Read WHO_AM_I using polling */
+	if (HAL_I2C_Mem_Read(&hi2c1,
+											 MPU6500_ADDRESS,
+											 WHO_AM_I_MPU6500,
+											 I2C_MEMADD_SIZE_8BIT,
+											 &readData,
+											 1,
+											 100) == HAL_OK)
+	{
+			snprintf(msg, sizeof(msg), "WHO_AM_I = 0x%02X\r\n", readData);
+			UART_Print(msg);
+
+			if (readData == 0x70)
+			{
+					UART_Print("MPU6500 detected.\r\n");
+			}
+			else
+			{
+					UART_Print("MPU6500 not detected.\r\n");
+			}
+	}
+	else
+	{
+			UART_Print("Read WHO_AM_I failed.\r\n");
+	}
+
+	/* Step 2: Write PWR_MGMT_1 using polling */
+	writeData = 0x00;
+
+	if (HAL_I2C_Mem_Write(&hi2c1,
+												MPU6500_ADDRESS,
+												PWR_MGMT_1,
+												I2C_MEMADD_SIZE_8BIT,
+												&writeData,
+												1,
+												100) == HAL_OK)
+	{
+			UART_Print("PWR_MGMT_1 write done.\r\n");
+	}
+	else
+	{
+			UART_Print("PWR_MGMT_1 write failed.\r\n");
+	}
+
+	/* Read back PWR_MGMT_1 using polling */
+	readData = 0;
+
+	if (HAL_I2C_Mem_Read(&hi2c1,
+											 MPU6500_ADDRESS,
+											 PWR_MGMT_1,
+											 I2C_MEMADD_SIZE_8BIT,
+											 &readData,
+											 1,
+											 100) == HAL_OK)
+	{
+			snprintf(msg, sizeof(msg), "PWR_MGMT_1 = 0x%02X\r\n\r\n", readData);
+			UART_Print(msg);
+	}
+	else
+	{
+			UART_Print("Read PWR_MGMT_1 failed.\r\n");
+	}
+
+	/* Step 3: Start first acceleration read using interrupt */
+	Start_Accel_Read_IT();
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    if (accelReady)
+    {
+        accelReady = 0;
+
+        accelX = (int16_t)(((uint16_t)accelData[0] << 8) | accelData[1]);
+        accelY = (int16_t)(((uint16_t)accelData[2] << 8) | accelData[3]);
+        accelZ = (int16_t)(((uint16_t)accelData[4] << 8) | accelData[5]);
+
+        snprintf(msg, sizeof(msg),
+                 "ACCEL_X = %d, ACCEL_Y = %d, ACCEL_Z = %d\r\n",
+                 accelX, accelY, accelZ);
+
+        UART_Print(msg);
+
+        HAL_Delay(500);
+
+        Start_Accel_Read_IT();
+    }
+
+    if (i2cError)
+    {
+        i2cError = 0;
+
+        UART_Print("I2C error occurred.\r\n");
+
+        HAL_Delay(1000);
+
+        Start_Accel_Read_IT();
+    }
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief UART5 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_UART5_Init(void)
+{
+
+  /* USER CODE BEGIN UART5_Init 0 */
+
+  /* USER CODE END UART5_Init 0 */
+
+  /* USER CODE BEGIN UART5_Init 1 */
+
+  /* USER CODE END UART5_Init 1 */
+  huart5.Instance = UART5;
+  huart5.Init.BaudRate = 115200;
+  huart5.Init.WordLength = UART_WORDLENGTH_8B;
+  huart5.Init.StopBits = UART_STOPBITS_1;
+  huart5.Init.Parity = UART_PARITY_NONE;
+  huart5.Init.Mode = UART_MODE_TX_RX;
+  huart5.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart5.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN UART5_Init 2 */
+
+  /* USER CODE END UART5_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
